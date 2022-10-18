5b7d6521f2b26b81f26f983985918af87701039b
Hukka <rhlehfndvkd7557@daum.net>
2022-10-18 17:03:42 +0900

    init : spring boot gradle setting initialize

---
+++ src/main/java/com/neuru/dotoribackend/DotoriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neuru.dotoribackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotoriBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotoriBackendApplication.class, args);
+    }
+
+}
