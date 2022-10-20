f6cb6e6e2d94e11c9f643ec3fbaefea58ef31156
Hukka <rhlehfndvkd7557@daum.net>
2022-10-20 18:14:14 +0900

    feat : feat fcm gradle setting

---
+++ src/main/java/com/neuru/dotoribackend/config/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package com.neuru.dotoribackend.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class FirebaseConfig {
+
+    @Value("${fcm.secret}")
+    private String fcmSecretKeyPath;
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+            FileInputStream serviceAccount = new FileInputStream(fcmSecretKeyPath);
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+        return FirebaseApp.initializeApp(options);
+    }
+}
